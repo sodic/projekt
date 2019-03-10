78088d300dedeaa1c87e0962c5277a38003c8a73
Filip Sodić <filip.sodic@fer.hr>
2019-03-10 17:13:47 +0100

    dodaj cudo

--- main.c
+++ main.c
@@ -4,8 +4,13 @@ void deri() {
     printf("Jel deres ti?\n");
 }
 
+void cudo() {
+    printf("jako veliko cudo");
+}
+
 int main() {
 	printf("Jako dobar, mozda cak predobar\n");
+        cudo();
         deri();
 	return 0;
 }
